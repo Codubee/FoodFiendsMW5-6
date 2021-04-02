1b005e1cc8b32e59ae0f7b2e5dee9730e0fc5fc5
posadari <57573681+posadari@users.noreply.github.com>
2021-04-02 15:29:57 -0500

    MW-3 features (#4)
    * Added features component with css styling
    
    * Deleted unused code
    
    * Rework for PR comments
    
    * Renamed files and moved the images folder
    
    Co-authored-by: codubee <info@codubee.com>

---
+++ frontend/src/components/features.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import img1 from '../images/review.svg'
+import img2 from '../images/list.svg'
+import img3 from '../images/eating.svg'
+import '../styles/Features.css'
+
+function Feature(props) {
+    return(
+        <div className="card text-center">
+            <div className="overflow">
+                <img src={props.imgsrc} alt="process" className="card-img-top"></img>
+            </div>
+            <div className="card-body text-dark">
+                <h4 className="card-title">{props.title}</h4>
+                <p className="card-text text-secondary">
+                {props.desc}
+                </p>
+            </div>
+        </div>
+    );
+}
+
+function Features() {
+    return (
+        <div>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-md-4">
+                        <Feature imgsrc={img1} title="Searching" desc="Figuring out what to eat by selecting food categories"/>
+                    </div>
+                    <div className="col-md-4">
+                        <Feature imgsrc={img2} title="Matching" desc="Finding a match between people's food choices"/>
+                    </div>
+                    <div className="col-md-4">
+                        <Feature imgsrc={img3} title="Choosing" desc="Making the hard choice when no one wants to eat the same thing"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    ) 
+}
+
+export default Features;
