c15f7cb33e68c5536fe0deced83a48a10beab06e
AlexanderdeBont <57573396+AlexanderdeBont@users.noreply.github.com>
2021-04-02 15:36:59 -0500

    MW-2 Add Explanation component (#3)
    * Added the explanation component and an image for a background. Image is more pink than intended, but should still be close enough to the color scheme.
    
    * This version does not compile due to the image path in Explanation.css being incorrect
    
    * Changed comment location
    
    changed comment position so it doesnt cause an error itself
    
    * Changed code according to requested change on pull review
    
    * removed comments, moved margin to paragraph
    
    Co-authored-by: codubee <info@codubee.com>
    Co-authored-by: Codubee <70549347+Codubee@users.noreply.github.com>

--- frontend/src/components/Explanation.js
+++ frontend/src/components/Explanation.js
@@ -1,1 +1,21 @@
-// This is where the explanation component will be created
+import React from 'react';
+import { Jumbotron} from 'reactstrap';
+import '../styles/Explanation.css';
+
+
+//Jumbotron component
+function Explanation(props) {
+  return (
+    <div>
+      <Jumbotron className="jumbo">
+        <h1 className="display-3 text-center text-white font-weight-bold">Welcome to Food Fiends!</h1>
+        <p className="lead text-center text-white">Food Fiends is for groups of friends who can't decide what to eat! Users can get together and can each choose 3 food types that
+                                                    they want from a list. The food type that the group of users pick the most is what's chosen as the winner for them to eat!</p>
+        <hr className="my-2" />
+        <p className="text-center text-white all-around-margin" >Codubee MW5-6, Spring 2021</p>
+      </Jumbotron>
+    </div>
+  );
+};
+
+export default Explanation
