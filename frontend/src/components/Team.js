246c2e922ecc32d16699afe65546f3f722bdf77b
LenaMubarak <54430114+LenaMubarak@users.noreply.github.com>
2021-04-11 17:08:59 -0500

    [MW-4] Adding team component with everyone's information (#5)
    * Adding team component with everyone's information
    
    * removed useless comments
    
    * Created stylesheet, removed room from landing page
    
    * fixed team card spacing issue
    
    Co-authored-by: codubee <info@codubee.com>
    Co-authored-by: Codubee <70549347+Codubee@users.noreply.github.com>

---
+++ frontend/src/components/Team.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import lena from '../images/lena.jpg'
+import alex from '../images/alex.png'
+import arielle from '../images/arielle.jpg'
+import hannah from '../images/hannah.jpg'
+import arath from '../images/arath.jpg'
+import '../styles/Team.css';
+
+
+import{
+    Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, CardDeck
+} from 'reactstrap'
+
+function team() {
+    return(
+        <div>
+            <CardDeck className="team-card-deck">
+
+
+                <Card className= "team-card" >
+                    <CardImg id="img" top width="100%" src={lena} alt="lena"/>
+                    <CardBody id="text">
+                        <CardTitle id="title">Lena Mubarak</CardTitle>
+                        <CardSubtitle >Software Engineering</CardSubtitle> 
+                        <CardText>The University of Texas at Dallas <p>Junior</p> </CardText>
+                    </CardBody>
+                </Card>
+
+
+                <Card className= "team-card" >
+                    <CardImg id="img" top width="100%" src={alex} alt="alex"/>
+                    <CardBody id="text">
+                        <CardTitle id="title">Alexander de Bont </CardTitle>
+                        <CardSubtitle >Computer Science</CardSubtitle> 
+                        <CardText>The University of Texas at Dallas <p>Undergraduate</p> </CardText>
+                    </CardBody>
+                </Card>
+
+
+                <Card className= "team-card" >
+                    <CardImg id="img" top width="100%" src={arielle} alt="arielle"/>
+                    <CardBody id="text">
+                        <CardTitle id="title">Arielle Posadas</CardTitle>
+                        <CardSubtitle >Computer Science</CardSubtitle> 
+                        <CardText>The University of Texas at Dallas <p>Sophomore</p> </CardText>
+                    </CardBody>
+                </Card>
+
+
+                <Card className= "team-card" >
+                    <CardImg id="img" top width="100%" src={hannah} alt="hannah"/>
+                    <CardBody id="text">
+                        <CardTitle id="title">Hannah Eason</CardTitle>
+                        <CardSubtitle >Computer Science</CardSubtitle> 
+                        <CardText>The University of Texas at Dallas <p>Undergraduate</p> </CardText>
+                    </CardBody>
+                </Card>
+                <Card className= "team-card" >
+                    <CardImg id="img" top width="100%" src={arath} alt="arath"/>
+                    <CardBody id="text">
+                        <CardTitle id="title">Arath Paneet</CardTitle>
+                        <CardSubtitle >Mechanical Engineering</CardSubtitle> 
+                        <CardText>The University of Texas at Dallas <p>Alumnus</p> </CardText>
+                    </CardBody>
+                </Card>
+            </CardDeck>
+        </div>
+    )
+}
+
+export default team;
--- frontend/src/views/LandingPage.js
+++ frontend/src/views/LandingPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Team from "../components/Team"
 import Explanation from '../components/Explanation'
 import Features from '../components/Features'
 
@@ -7,6 +8,7 @@ function LandingPage(){
         <div>
             <Explanation />
             <Features/>
+            <Team/>
         </div>
     )
 }
