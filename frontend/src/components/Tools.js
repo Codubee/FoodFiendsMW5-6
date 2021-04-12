91f69e64293291a17c5543e52962dae83e7a9e1a
codubee <info@codubee.com>
2021-04-12 17:00:28 -0500

    Fixed spacing issues with iamages

--- frontend/src/components/Tools.js
+++ frontend/src/components/Tools.js
@@ -1,11 +1,11 @@
 import React from 'react'
+import {Row, Col} from 'reactstrap'
 import img1 from '../images/react.svg'
 import img2 from '../images/nodejs.svg'
 import img3 from '../images/jira.svg'
 import img4 from '../images/postman.svg'
 import img5 from '../images/aws.svg'
 import img6 from '../images/heroku.svg'
-import '../styles/Tools.css' 
 
 // Tools Component 
 
@@ -13,28 +13,26 @@ function Tools() {
     return (
         <div>
             <div className="container-fluid">
-                <div className="row">
-                    <div classname="img">
-                    <div className="col-1">
+                <Row >
+                    <Col>
                         <img src={img1} title="SReact" desc="SReact"/>
-                    </div>
-                    <div className="col-1">
+                    </Col>
+                    <Col  >
                         <img src={img2} title="NodeJS" desc="NodeJS"/>
-                    </div>
-                    <div className="col-1">
+                    </Col>
+                    <Col  >
                         <img src={img3} title="Jira" desc="Jira"/>
-                    </div>
-                    <div className="col-1">
+                    </Col>
+                    <Col  >
                         <img src={img4} title="Postman" desc="Postman"/>
-                    </div>
-                    <div className="col-1">
+                    </Col>
+                    <Col  >
                         <img src={img5} title="AWS" desc="AWS"/>
-                    </div>
-                    <div className="col-1">
+                    </Col>
+                    <Col  >
                         <img src={img6} title="Heroku" desc="Heroku"/>
-                    </div>
-                    </div>
-                </div>
+                    </Col>
+                </Row>
             </div>
         </div>
     ) 
