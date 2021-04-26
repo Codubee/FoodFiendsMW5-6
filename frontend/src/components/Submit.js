b18a0afb12447edda513850549b79dc8555fe447
LenaMubarak <layoonamubarak@gmail.com>
2021-04-25 21:43:44 -0500

    adding a submit button for categories

---
+++ frontend/src/components/Submit.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import {Button} from 'reactstrap'
+
+function submitButton(){
+    return <Button outline color="success" size='lg' >Submit</Button>
+}
+
+export default submitButton;
