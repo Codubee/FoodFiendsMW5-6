9451677d50ff1b4478ef0c9528b2f8b4174db1c3
axp7186 <arathpaneet@gmail.com>
2021-04-28 17:00:00 -0500

    Refresh Button

---
+++ frontend/src/components/RefreshButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+    var btn = document.createElement("REFRESH BUTTON");
+    btn.innerHTML = "Refresh";
+
+    var body = document.getElementsByTagName("body")
+    [0];
+    body.appendChild(button);
+
+
