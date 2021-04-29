fc2706a0bd9fe5fa7ea69d30d7c2b8affc4adbd3
codubee <info@codubee.com>
2021-04-28 21:17:16 -0500

    Added categories to the match page

--- frontend/src/views/Match.js
+++ frontend/src/views/Match.js
@@ -1,12 +1,19 @@
 import React from 'react'
-import Categories from '../components/Categories'
-import {Button, Container} from 'reactstrap'
+import { Button, Container } from 'reactstrap'
 import '../styles/Match.css'
 
-function Match(){
-    return(
+function Match() {
+    return (
         <Container className="text-center mt-lg">
-            <Categories/>
+            <div>
+                <label for="categories">Choose a food category: </label>
+                <select name="categories" id="categories">
+                    <option value="italian">Italian</option>
+                    <option value="mexican">Mexican</option>
+                    <option value="indian">Indian</option>
+                    <option value="chinese">Chinese</option>
+                </select>
+            </div>
             <Button className="mt-sm" outline color="success" size='lg' >Submit</Button>
         </Container>
     )
