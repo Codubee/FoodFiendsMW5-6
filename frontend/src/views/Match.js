d38b57e0e79c0f0dd043842a781559a0afc9236b
Hana Eason <51489485+HanaEason@users.noreply.github.com>
2021-04-28 21:17:43 -0500

    MW - 8 - categories (#11)
    * categories component
    
    * changed category formatting
    
    * css styling for categories
    
    * Added margins
    
    * Added categories to the match page
    
    Co-authored-by: Codubee <70549347+Codubee@users.noreply.github.com>
    Co-authored-by: codubee <info@codubee.com>

--- frontend/src/views/Match.js
+++ frontend/src/views/Match.js
@@ -1,11 +1,20 @@
 import React from 'react'
-import {Button, Container} from 'reactstrap'
+import { Button, Container } from 'reactstrap'
 import '../styles/Match.css'
 
-function Match(){
-    return(
-        <Container className="text-center pt-lg">
-            <Button outline color="success" size='lg' >Submit</Button>
+function Match() {
+    return (
+        <Container className="text-center mt-lg">
+            <div>
+                <label for="categories">Choose a food category: </label>
+                <select name="categories" id="categories">
+                    <option value="italian">Italian</option>
+                    <option value="mexican">Mexican</option>
+                    <option value="indian">Indian</option>
+                    <option value="chinese">Chinese</option>
+                </select>
+            </div>
+            <Button className="mt-sm" outline color="success" size='lg' >Submit</Button>
         </Container>
     )
 }
