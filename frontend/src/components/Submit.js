c67bb0dba336211ab475221a053cb9f320198688
codubee <info@codubee.com>
2021-04-28 21:08:05 -0500

    Moved button to match component

--- frontend/src/components/Submit.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import {Button} from 'reactstrap'
-
-function submitButton(){
-    return <Button outline color="success" size='lg' >Submit</Button>
-}
-
-export default submitButton;
