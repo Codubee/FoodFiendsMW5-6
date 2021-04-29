4bd4dcd9ef3927e3c507e653f8f24fcd9c28c5da
LenaMubarak <54430114+LenaMubarak@users.noreply.github.com>
2021-04-28 21:09:05 -0500

    [MW-13] adding a submit button for categories (#10)
    * adding a submit button for categories
    
    * Moved button to match component
    
    Co-authored-by: codubee <info@codubee.com>
    Co-authored-by: Codubee <70549347+Codubee@users.noreply.github.com>

--- frontend/src/views/Match.js
+++ frontend/src/views/Match.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import {Button, Container} from 'reactstrap'
+import '../styles/Match.css'
 
 function Match(){
     return(
-        <></>
+        <Container className="text-center pt-lg">
+            <Button outline color="success" size='lg' >Submit</Button>
+        </Container>
     )
 }
 
