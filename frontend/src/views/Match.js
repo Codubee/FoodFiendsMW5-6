e6d8587c01c842dc717b158cd9860211c164524e
AlexanderdeBont <57573396+AlexanderdeBont@users.noreply.github.com>
2021-06-06 20:23:55 -0500

    MW-19-addchoiceAPI (#15)
    * Bug fixes and added callAddChoice method
    
    * Removed double axios import
    
    Co-authored-by: Codubee <70549347+Codubee@users.noreply.github.com>
    Co-authored-by: codubee <info@codubee.com>

--- frontend/src/views/Match.js
+++ frontend/src/views/Match.js
@@ -1,14 +1,15 @@
 import React from 'react'
-import axios from 'axios'
 import { Button, Container } from 'reactstrap'
 import '../styles/Match.css'
+const axios = require('axios');
 
 class Match extends React.Component {
 
     constructor(props){
         super(props)
-        this.state = {id:0,category:''}
+        this.state = {id:0,category:'italian'}
         this.handleCategoryChange = this.handleCategoryChange.bind(this)
+        this.callAddChoice = this.callAddChoice.bind(this)
     }
     
     componentDidMount() {
@@ -18,7 +19,6 @@ class Match extends React.Component {
     callCreateRoomAPI() {
         axios.get('/createRoom?id=' + this.createRoomId())
             .then((response) => {
-                console.log(response.data);
                 this.setState({id:response.data.id})
             })
     }
@@ -27,9 +27,16 @@ class Match extends React.Component {
         this.setState({category:event.target.value})
     }
 
+    //Dont want room id for this instance to be random. Change
     createRoomId(){
         return Math.floor(Math.random() * 1000) + 1;
     }
+
+    //This calls the backend with the current state of the match instance (Including the room id and the choice of food type)
+    callAddChoice(){
+        axios.post('/addChoice', this.state)
+    }
+
     render() {
         return (
             <Container className="text-center mt-lg">
@@ -42,7 +49,7 @@ class Match extends React.Component {
                         <option value="chinese">Chinese</option>
                     </select>
                 </div>
-                <Button className="mt-sm" outline color="success" >Submit</Button>
+                <Button className="mt-sm" outline color="success" onClick={this.callAddChoice}>Submit</Button>
                 <Button className="mt-sm ml-sm" outline>Refresh</Button>
             </Container>
         )
